d47a956a409b4fdafa1fcc4a0eeb09092d7d9a0e
51222019 <Adira@072025>
2025-07-25 10:49:31 +0700

    push perbaikan menu result setelah scan QR dan nambah menu untuk download rekap kehadiran tamu

--- src/main/java/id/api/controller/ScanQrController.java
+++ src/main/java/id/api/controller/ScanQrController.java
@@ -5,9 +5,11 @@ import id.api.service.QrCodeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ScanQrController {
@@ -24,5 +26,14 @@ public class ScanQrController {
     public ResponseEntity<String> saveScan(@RequestBody ScanRequest request) {
         qrCodeService.saveScanToExcel(request.getHasil(), request.getWaktu());
         return ResponseEntity.ok("Disimpan ke Excel");
+    }
+
+    @GetMapping("/result")
+    public String showResult(@RequestParam("nama") String nama,
+                             @RequestParam("waktu") String waktu,
+                             Model model) {
+        model.addAttribute("hasil", nama);
+        model.addAttribute("waktu", waktu);
+        return "result";
     }
 }
